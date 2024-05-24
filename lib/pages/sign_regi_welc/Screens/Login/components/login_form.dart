ef1f4048a3d8d78dd0f380d6930cfe53e6b31e0f
hao <547783993@qq.com>
2024-05-24 23:32:57 +0800

    connect with backend user

--- lib/pages/sign_regi_welc/Screens/Login/components/login_form.dart
+++ lib/pages/sign_regi_welc/Screens/Login/components/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:comp7705_chatbot/pages/home_page.dart';
+import 'package:comp7705_chatbot/service/auth_service.dart';
 import 'package:flutter/material.dart';
 
 import '../../../components/already_have_an_account_acheck.dart';
@@ -18,19 +19,32 @@ class _LoginFormState extends State<LoginForm> {
   final _formKey = GlobalKey<FormState>();
   String? _username;
   String? _password;
+  bool _isObscure = true;
+  //final _authService = AuthService();
 
-  void _onLogin() {
+  Future<void> _onLogin() async {
     if (_formKey.currentState?.validate() ?? false) {
       _formKey.currentState?.save();
       // Perform login logic with _username and _password
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) {
-            return  HomePage(username: _username!);
-          },
-        ),
-      );
+      try {
+        final tokens = await AuthService.login(_username!, _password!);
+        // 登录成功后，导航到主页面
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return HomePage(username: _username!);
+            },
+          ),
+        );
+      } catch (e) {
+        // 处理登录失败的情况
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Login failed: $e'),
+          ),
+        );
+      }
     }
   }
 
@@ -61,15 +75,25 @@ class _LoginFormState extends State<LoginForm> {
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
             child: TextFormField(
               textInputAction: TextInputAction.done,
-              obscureText: true,
+              obscureText: _isObscure,
               cursorColor: mainColor,
               onSaved: (password) => _password = password,
               validator: (value) => value?.isEmpty ?? true ? 'Please enter a password' : null,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 hintText: "Your password",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
                   child: Icon(Icons.lock),
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isObscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                  onPressed: () {
+                    setState(() {
+                      _isObscure = !_isObscure;
+                    });
+                  },
                 ),
               ),
             ),
--- lib/pages/sign_regi_welc/Screens/Signup/components/signup_form.dart
+++ lib/pages/sign_regi_welc/Screens/Signup/components/signup_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
+import 'package:comp7705_chatbot/service/auth_service.dart';
 import '../../../components/already_have_an_account_acheck.dart';
 import '../../Login/login_screen.dart';
 import 'package:comp7705_chatbot/const.dart';
@@ -15,15 +16,66 @@ class SignUpForm extends StatefulWidget {
 
 class _SignUpFormState extends State<SignUpForm> {
   final _formKey = GlobalKey<FormState>();
-  String? _username;
-  String? _email;
-  String? _password;
-  String? _confirmPassword;
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  bool _isObscure = true;
+
+  bool _isLoading = false;
+
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   void _onSaved() {
     if (_formKey.currentState?.validate() ?? false) {
-      _formKey.currentState?.save();
-      // Perform sign-up logic with _username, _email, _password
+      // Perform sign-up logic with the values from the controllers
+      String username = _usernameController.text.trim();
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+      String confirmPassword = _confirmPasswordController.text.trim();
+      // Do something with the collected data
+    }
+  }
+  Future<void> _handleRegister() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      setState(() {
+        _isLoading = true;
+      });
+      try {
+        await AuthService.register(
+          _usernameController.text,
+          _passwordController.text,
+          _emailController.text,
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Registration successful!'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+        // 注册成功后的处理逻辑
+        // 例如: 跳转到登录页面或显示成功提示
+        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+      } catch (e) {
+        // 注册失败的错误处理
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Registration failed: $e'),
+          ),
+        );
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
   }
 
@@ -34,12 +86,12 @@ class _SignUpFormState extends State<SignUpForm> {
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding/2),
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding / 2),
             child: TextFormField(
+              controller: _usernameController,
               keyboardType: TextInputType.text,
               textInputAction: TextInputAction.next,
               cursorColor: mainColor,
-              onSaved: (username) => _username = username,
               validator: (value) => value?.isEmpty ?? true ? 'Please enter a username' : null,
               decoration: const InputDecoration(
                 hintText: "Your username",
@@ -51,13 +103,13 @@ class _SignUpFormState extends State<SignUpForm> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding/2),
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding / 2),
             child: TextFormField(
+              controller: _emailController,
               keyboardType: TextInputType.emailAddress,
               textInputAction: TextInputAction.next,
               cursorColor: mainColor,
-              onSaved: (email) => _email = email,
-              validator: (value) => value?.isEmpty ?? true ? 'Please enter an email' : null,
+              //validator: (value) => value?.isEmpty ?? true ? 'Please enter an email' : null,
               decoration: const InputDecoration(
                 hintText: "Your email",
                 prefixIcon: Padding(
@@ -68,42 +120,74 @@ class _SignUpFormState extends State<SignUpForm> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding/2),
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding / 2),
             child: TextFormField(
+              controller: _passwordController,
               textInputAction: TextInputAction.next,
-              obscureText: true,
+              obscureText: _isObscure,
               cursorColor: mainColor,
-              onSaved: (password) => _password = password,
-              validator: (value) => value?.isEmpty ?? true ? 'Please enter a password' : null,
-              decoration: const InputDecoration(
+              //validator: (value) => value?.isEmpty ?? true ? 'Please enter a password' : null,
+              validator: (value) {
+                if (value?.isEmpty ?? true) {
+                  return 'Please enter a password';
+                } else if (value!.length < 8) {
+                  return 'Password must be at least 8 characters long';
+                } else if (!RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+                    .hasMatch(value)) {
+                  return 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
                 hintText: "Your password",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
                   child: Icon(Icons.lock),
                 ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isObscure ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isObscure = !_isObscure;
+                    });
+                  },
+                ),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding/2),
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding / 2),
             child: TextFormField(
+              controller: _confirmPasswordController,
               textInputAction: TextInputAction.done,
-              obscureText: true,
+              obscureText: _isObscure,
               cursorColor: mainColor,
-              onSaved: (confirmPassword) => _confirmPassword = confirmPassword,
               validator: (value) {
                 if (value?.isEmpty ?? true) {
                   return 'Please confirm your password';
-                } else if (value != _password) {
+                }
+                if (_passwordController.text != value) {
                   return 'Passwords do not match';
                 }
                 return null;
               },
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 hintText: "Confirm your password",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
                   child: Icon(Icons.lock),
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isObscure ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isObscure = !_isObscure;
+                    });
+                  },
                 ),
               ),
             ),
@@ -112,7 +196,7 @@ class _SignUpFormState extends State<SignUpForm> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
             child: ElevatedButton(
-              onPressed: _onSaved,
+              onPressed: _handleRegister,
               child: Text("Sign Up".toUpperCase()),
             ),
           ),
